0ead146bb0486f23d07eceff899eb1b1e1e7d096
OrneM <ornemans88@gmail.com>
2026-01-06 18:01:53 -0300

    implement search flow in searchProduct (type + Enter + close)

--- src/test/java/edit/Prueba1.java
+++ src/test/java/edit/Prueba1.java
@@ -1,8 +1,13 @@
 package edit;
 
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import static org.openqa.selenium.WebElement.*;
 
 public class Prueba1 {
 
@@ -20,10 +25,13 @@ public class Prueba1 {
         browser.get("http://www.automationpractice.pl/index.php");
 
     // 4- write the product you want to search for (text field)
+        WebElement txtSearch = browser.findElement(By.id("search_query_top"));
+        txtSearch.sendKeys("dress");
 
     // 5- simulate pressing enter
+        txtSearch.sendKeys(Keys.ENTER);
 
     // 6- close the navigator
-
+        browser.close();
     }
 }
